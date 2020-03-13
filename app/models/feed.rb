5a5f4e5c2e276a90e39d408964b6541b792926df
Joe Mahoney <jmahoney@iterative.co.nz>
2020-03-13 22:49:12 +1300

    change the url if the source url has changed

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -8,14 +8,18 @@ class Feed < ApplicationRecord
     response = HTTParty.get(self.url, format: :plain)
     if response.code == 200
       parsed_feed = Feedjira.parse(response.body)
-      update_feed_details(parsed_feed.title)
+      update_feed_details(parsed_feed.title, response.request)
     end
   end
 
   private 
-  def update_feed_details(name)
+  def update_feed_details(name, request)
     if self.name != name
       self.name = name
+    end
+
+    if (request.redirect && request.uri != self.url)
+      self.url = request.uri
     end
 
     if self.changed?
--- test/models/feed_test.rb
+++ test/models/feed_test.rb
@@ -3,14 +3,26 @@ require 'mocha/minitest'
 
 class FeedTest < ActiveSupport::TestCase
   def setup
-    @feed = Feed.first
+    @new_feed_url = 'https://test.com/feed.xml'
+    @response = stub(code: 200, 
+          body: file_fixture('cheerschopper_rss.xml').read,
+          request: stub(redirect: true, uri: @new_feed_url))
+    
   end
 
+
   test "name is updated if the source feed name has changed" do
-    response = stub(code: 200, body: file_fixture('cheerschopper_rss.xml').read)
-    HTTParty.expects(:get).with(@feed.url, format: :plain).returns(response)
-    @feed.fetch
-    assert_equal "cheers, chopper", @feed.name
+    feed = Feed.first
+    HTTParty.expects(:get).with(feed.url, format: :plain).returns(@response)
+    feed.fetch
+    assert_equal "cheers, chopper", feed.name
+  end
+
+  test "feed url is changed if source feed url has changed" do
+    feed = Feed.last
+    HTTParty.expects(:get).with(feed.url, format: :plain).returns(@response)
+    feed.fetch
+    assert_equal @new_feed_url, feed.url
   end
 
 end
