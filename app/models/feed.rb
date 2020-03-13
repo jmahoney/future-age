46a43fade94458f218bf37cd34d26a5aac849c9d
Joe Mahoney <jmahoney@iterative.co.nz>
2020-03-13 22:31:08 +1300

    testing with mocha

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -3,4 +3,24 @@ class Feed < ApplicationRecord
   has_many :fetches
 
   enum status: {active: 0, flaky: 2, inactive: 3}
+
+  def fetch
+    response = HTTParty.get(self.url, format: :plain)
+    if response.code == 200
+      parsed_feed = Feedjira.parse(response.body)
+      update_feed_details(parsed_feed.title)
+    end
+  end
+
+  private 
+  def update_feed_details(name)
+    if self.name != name
+      self.name = name
+    end
+
+    if self.changed?
+      self.save
+    end
+  end
+
 end
--- test/models/feed_test.rb
+++ test/models/feed_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
+require 'mocha/minitest'
 
 class FeedTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @feed = Feed.first
+  end
+
+  test "name is updated if the source feed name has changed" do
+    response = stub(code: 200, body: file_fixture('cheerschopper_rss.xml').read)
+    HTTParty.expects(:get).with(@feed.url, format: :plain).returns(response)
+    @feed.fetch
+    assert_equal "cheers, chopper", @feed.name
+  end
+
 end
