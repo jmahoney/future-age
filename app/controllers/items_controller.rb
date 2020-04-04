cdc95036f8884deb458aab4cf1cf0ef4618b0e70
Joe Mahoney <jmahoney@iterative.co.nz>
2020-04-05 07:02:45 +1200

    wip on marking as starred

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,10 @@ class ItemsController < ApplicationController
   def index
     @items = Item.includes(:feed).where(read: false).order(date_published: :desc).limit(30)
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:feed_id, :read, :starred)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,12 @@ Rails.application.routes.draw do
     resources :feeds
   end
 
-  resources :items, only: [:index, :update]
+  resources :items, only: [:index, :update] do
+    member do
+      patch 'toggle_starred'
+      patch 'toogle_read'
+    end
+  end
 
   root 'items#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
