0cfac90d531d893e2f9d7a02f9dfed917ee3ad98
Joe Mahoney <jmahoney@iterative.co.nz>
2020-04-06 21:37:36 +1200

    faffing around with js. should not need to do this!

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,7 +11,9 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("clean-blog")
+require("jquery")
+//require("clean-blog")
+require("fa")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
