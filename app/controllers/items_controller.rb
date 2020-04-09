5204b44390f133f357538aa9820143981424d0be
Joe Mahoney <jmahoney@iterative.co.nz>
2020-04-09 17:19:39 +1200

    paginate by start date

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,9 @@
 class ItemsController < ApplicationController
   def index
-    @items = Item.includes(:feed).where(read: false).order(date_published: :desc).limit(30)
+    @items = Item.includes(:feed)
+                .where("date_published < ?", start_date(params[:s]))
+                .where(read: false)
+                .order(date_published: :desc).limit(3)
   end
 
   def toggle_starred
@@ -11,6 +14,17 @@ class ItemsController < ApplicationController
       else
         format.json { render json: @item.errors, status: :unprocessable_entity }
       end
+    end
+  end
+
+  private
+  def start_date(d)
+    return DateTime.now.utc if d.nil?
+
+    begin
+      return DateTime.parse(d)
+    rescue => exception
+      return DateTime.now.utc
     end
   end
 
