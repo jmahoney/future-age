707fe557199c3fc941a904aa6b8825ecea287c78
Joe Mahoney <jmahoney@iterative.co.nz>
2020-04-09 17:57:04 +1200

    need to attach the event listener every time turbolinks loads

--- app/javascript/fa.js
+++ app/javascript/fa.js
@@ -1,4 +1,4 @@
-$( document ).ready(function() {
+$( document ).on("turbolinks:load",function() {
   document.body.addEventListener('ajax:success', function(event) {
     var detail = event.detail;
     var data = detail[0], status = detail[1], xhr = detail[2];
